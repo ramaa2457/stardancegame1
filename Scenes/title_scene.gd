72cf8cb19dba77a477e27b254aea48039d708dde
ramaa2457 <ghaniarahli2@gmail.com>
2026-09-02 19:40:01 +1000

    basic versions of start, settings and quit buttons added

---
+++ Scenes/title_scene.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/settings_scene.tscn")
+	pass # Replace with function body.
